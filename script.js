7dc985d1887f9998d634d08ce5d1725fa93c279c
Ocklon <ocklondev@outlook.com>
2023-06-14 10:43:00 -0500

    modified:   index.html 	modified:   script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function initializeAutocomplete() {
-    var input = document.getElementById('address-input');
-    var options = {
-      types: ['geocode'] // Restrict the results to addresses only
-    };
-    var autocomplete = new google.maps.places.Autocomplete(input, options);
-  }
-  
-  google.maps.event.addDomListener(window, 'load', initializeAutocomplete);
